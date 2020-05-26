9ea495ccf9a3056e39938ea843d3c106cb107ce4
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-26 18:26:22 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+if (process.env.MODE === "DEV") {
+    const path = require("path");
+    require('dotenv').config({ path: path.join(__dirname, ".env") })
+}
+
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
